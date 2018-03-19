d706a2e72e090557b2316692b1fcbc7e0fb6d8e9
Rliushi <1066307152@qq.com>
2018-03-19 22:56:13 +0800

    image from db

--- ClassQuery/models.py
+++ ClassQuery/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.utils.encoding import python_2_unicode_compatible
 # Create your models here.
+
+
+@python_2_unicode_compatible
+class ClassImage(models.Model):
+    classID = models.IntegerField()
+    className = models.CharField(max_length=20)
+    sectionID = models.IntegerField()
+    imageSeq = models.IntegerField()
+    imagePath = models.CharField(max_length=100)
--- ClassQuery/views.py
+++ ClassQuery/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import os
+from .models import ClassImage
 # Create your views here.
 
 
@@ -12,13 +13,18 @@ def test(request):
     return render(request, 'ClassQuery/HiMath7_1.html')
 
 
-def getimages(request):
-        section_id = request.GET['section']
-        curr_dir = os.path.dirname(os.path.dirname(__file__))
-        image_dir = os.path.join(curr_dir, 'ClassQuery', 'static', 'images', section_id)
-        count = len(os.listdir(image_dir))
-        image_info = list()
-        for i in range(count):
-            image_info.append("static/images/" + section_id + "/" + section_id.replace("section_", "") + "-" + str(i + 1) + ".png")
-        return render(request, 'ClassQuery/HiMath7_1.html', {'image_info': image_info})
+# def getimages(request):
+#         section_id = request.GET['section']
+#         curr_dir = os.path.dirname(os.path.dirname(__file__))
+#         image_dir = os.path.join(curr_dir, 'ClassQuery', 'static', 'images', section_id)
+#         count = len(os.listdir(image_dir))
+#         image_info = list()
+#         for i in range(count):
+#             image_info.append("static/images/" + section_id + "/" + section_id.replace("section_", "") + "-" + str(i + 1) + ".png")
+#         return render(request, 'ClassQuery/HiMath7_1.html', {'image_info': image_info})
 
+def getimages(request):
+    section_id = request.GET['section']
+    image_info = ClassImage.objects.filter(sectionID=section_id).order_by('imageSeq')
+
+    return render(request, 'ClassQuery/HiMath7_1.html', {'image_info': image_info})
