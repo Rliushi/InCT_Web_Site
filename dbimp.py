d2909d9d031b9c8b647414159aabd0d87da6332e
Rliushi <1066307152@qq.com>
2018-04-02 20:49:21 +0800

    bug fix

--- dbimp.py
+++ dbimp.py
@@ -10,12 +10,14 @@ django.setup()
 
 def main():
     from ClassQuery.models import ClassImage
-    ls = os.listdir('F:\DjangoWork\InCT_Web_Site\collected_static\images\math')
+    ls = os.listdir('F:\\DjangoWork\\InCT_Web_Site\\collected_static\images\\xd')
     i = 1
-    for item in ls :
-        ls_a = os.listdir(item)
+    for item in ls:
+        print(item)
+        ls_a = os.listdir(os.path.join('F:\\DjangoWork\\InCT_Web_Site\\collected_static\\images\\xd', item))
+        # print(ls_a)
         for image in ls_a:
-            ClassImage.objects.create(classID=2, className='xd', sectionID=int(item.split('_')[4]), imageSeq=i, imagePath=image)
+            ClassImage.objects.create(classID=3, className='xd', sectionID=int(item.split('_')[1]), imageSeq=i, imagePath=image)
             i += 1
     # XjhInfo.objects.create(city='乌鲁木齐', school='新疆大学', company='中国水利水电第四工程局', location='校本部众创空间一楼路演厅', time='20180402110000')
     # XjhInfo.objects.create(city='乌鲁木齐', school='新疆大学', company='中建二局基础设施建设', location='南校区地矿学院三楼会议室', time='20180402153000')
