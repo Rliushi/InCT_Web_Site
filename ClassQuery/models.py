e2c72b7f6a39879318321290de09962c0e5a8e65
Rliushi <1066307152@qq.com>
2018-04-02 19:43:32 +0800

    添加宣讲会查询

--- ClassQuery/models.py
+++ ClassQuery/models.py
@@ -10,3 +10,13 @@ class ClassImage(models.Model):
     sectionID = models.IntegerField()
     imageSeq = models.IntegerField()
     imagePath = models.CharField(max_length=100)
+
+
+@python_2_unicode_compatible
+class XjhInfo(models.Model):
+    city = models.CharField(max_length=20)
+    city_id = models.CharField(max_length=10, default='xj')
+    school = models.CharField(max_length=50)
+    company = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    time = models.CharField(max_length=14)
--- ClassQuery/views.py
+++ ClassQuery/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import os
 from .models import ClassImage
+from .models import XjhInfo
+from django.core import serializers
+import json
+from django.http import JsonResponse
 # Create your views here.
 
 
@@ -13,6 +17,9 @@ def test(request):
     return render(request, 'ClassQuery/HiMath7_1.html')
 
 
+def xjh(request):
+    return render(request, 'ClassQuery/XJH.html')
+
 # def getimages(request):
 #         section_id = request.GET['section']
 #         curr_dir = os.path.dirname(os.path.dirname(__file__))
@@ -23,8 +30,27 @@ def test(request):
 #             image_info.append("static/images/" + section_id + "/" + section_id.replace("section_", "") + "-" + str(i + 1) + ".png")
 #         return render(request, 'ClassQuery/HiMath7_1.html', {'image_info': image_info})
 
+
 def getimages(request):
     section_id = request.GET['section']
     image_info = ClassImage.objects.filter(sectionID=section_id).order_by('imageSeq')
 
     return render(request, 'ClassQuery/HiMath7_1.html', {'image_info': image_info})
+
+
+def xjh_query(request):
+    city_name = request.GET['city']
+    xjh_info = XjhInfo.objects.filter(city_id=city_name)
+    tmp_list = []
+    for item in xjh_info:
+        tmp_dict = dict()
+        tmp_dict['city'] = item.city
+        tmp_dict['school'] = item.school
+        tmp_dict['company'] = item.company
+        tmp_dict['location'] = item.location
+        tmp_dict['time'] = item.time
+        tmp_list.append(tmp_dict)
+    # data = serializers.serialize('json', xjh_info, ensure_ascii=False)
+    # data = json.dumps(tmp_list, ensure_ascii=False)
+    return JsonResponse(tmp_list, safe=False)
+    # return HttpResponse(data, content_type='application/json', charset='utf-8')
